f8ce7d4f5e4ed530ad8f4ddd86873d50f2c5b1ae
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-12-18 20:41:46 +0100

    Add notes

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ var command = process.argv[2]
 console.log('Command: ', command)
 
 if (command === 'add') {
-  console.log('Adding New Note')
+  notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-  console.log('List all notes')
+  notes.getAll()
 } else if (command === 'read') {
-  console.log('Reading notes')
+  notes.getNote(argv.title)
 } else if (command === 'remove') {
-  console.log('Removing note')  
+  notes.removeNote(argv.title) 
 } else {
   console.log('Command not recognized')
 }
--- notes.js
+++ notes.js
@@ -4,3 +4,26 @@ module.exports.addNote = () => {
   console.log('Add Note')
   return 'New Note'
 }
+
+var addNote = (title, body) => {
+  console.log('Adding Note', title, body)
+}
+
+var getAll = () => {
+  console.log('Getting all notes')
+}
+
+var getNote = (title) => {
+  console.log('Getting note', title)
+}
+
+var removeNote = (title) => {
+  console.log('Remove note', title)
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
